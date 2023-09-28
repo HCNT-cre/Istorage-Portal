78839d376a4a5761802203172f8a88a5aac7903b
nxhieu3102 <nguyenxuanhieuktb10@gmail.com>
2023-09-28 13:59:13 +0700

    Change project structure

--- src/pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-export default Home = () =>{
-    return (
-        <div>
-            <div className="w-full h-[calc(100vh-75px)] bg-red-500">
-                <p>Home</p>
-            </div>
-        </div>
-    )
-}
