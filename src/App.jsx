16de7b8a245a79002ae60cbabd85d0a62500c6fe
nxhieu3102 <nguyenxuanhieuktb10@gmail.com>
2023-10-01 00:12:00 +0700

    Add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
-
+import { BrowserRouter, Routes, Route, Navigate, useParams } from "react-router-dom";
+import Files from "./pages/Files";
 const App = () => {
 	return (
 		<div>
-			<Layout
-				children={<Home />}
-			/>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Layout
+						children={<Home />}
+					/>} />
+					<Route path="/van-ban" element={<Layout
+						children={<Files />}
+					/>} />
+				</Routes>
+			</BrowserRouter>
+
 		</div>
 	)
 };
