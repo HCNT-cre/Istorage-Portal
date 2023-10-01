4088ed8b965f21ee44392cd00b632f2d4218cc67
nxhieu3102 <nguyenxuanhieuktb10@gmail.com>
2023-10-02 03:08:58 +0700

    Add getDocumentById

--- src/service/api/DocumentAPIService.js
+++ src/service/api/DocumentAPIService.js
@@ -1,9 +1,7 @@
 
 import axiosHttpService from "src/utils/httpService";
-const API_GOV_FILE_GET_ALL = import.meta.env.VITE_API_GOV_FILE_GET_ALL;
-const API_GOV_FILE_SEARCH = import.meta.env.VITE_API_GOV_FILE_GET_ALL;
 const API_SEARCH = import.meta.env.VITE_API_SEARCH;
-
+const API_DOCUMENT_GET = import.meta.env.VITE_API_DOCUMENT_GET
 const DocumentAPIService = {
     getDocumentByTitle: async (title) => {
         const response = await axiosHttpService.post(`${API_SEARCH}`,
@@ -13,9 +11,10 @@ const DocumentAPIService = {
         return response.data;
     },
 
-    getDocumentById: async (id) => {
-        const response = await axiosHttpService.get(`${API_SEARCH}/${id}`)
-        return response.data;
+    getDocumentById: async (idFile, idDoc) => {
+        const response = await axiosHttpService.get(`${API_DOCUMENT_GET}${idFile}`)
+        const doc = response.data.filter((doc) => doc.id == idDoc);
+        return doc[0] || [];
     }
 }
 
