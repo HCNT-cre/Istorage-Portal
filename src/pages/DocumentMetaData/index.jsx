548239a63fbeb0933cf66b4b7272144ff462c089
nxhieu3102 <nguyenxuanhieuktb10@gmail.com>
2023-10-02 03:08:16 +0700

    Add logic for DocumentMetaData page

--- src/pages/DocumentMetaData/index.jsx
+++ src/pages/DocumentMetaData/index.jsx
@@ -1,7 +1,33 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom"
+import TableDetail from "src/components/TableDetail";
+import DocumentAPIService from "src/service/api/DocumentAPIService";
+import { FIELDS_TABLE_DOCUMENT_DETAIL } from "src/storage/DocumentStorage";
+
 const DocumentMetaData = () => {
+    const params = useParams();
+    const {idFile, id} = params;
+    const [doc, setDoc] = useState([])
+    const fieldData = []
+
+    for(const i in FIELDS_TABLE_DOCUMENT_DETAIL) {
+        const detail = FIELDS_TABLE_DOCUMENT_DETAIL[i]
+        fieldData.push({title: detail.title, value: doc[detail.key]})
+    }
+
+    useEffect(() =>{
+        const getDocById = async () => {
+            const response = await DocumentAPIService.getDocumentById(idFile, id)
+            setDoc(response)
+        }
+        getDocById();
+    }, [idFile, id])
+
     return (
-        <div>
-        </div>
+        <TableDetail
+            headText={"Thông tin văn bản"}
+            data={fieldData}
+        />
     )
 }
 
