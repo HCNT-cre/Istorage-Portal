67821a0290c6ca3ee4cc7b042870a5d499f458ee
HCNT-cre <cuonghung2k3@gmail.com>
2023-10-02 10:11:51 +0700

    create login and register form

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import { BrowserRouter, Routes, Route, Navigate, useParams } from "react-router-dom";
 import Files from "./pages/Files";
+import Login from "./pages/Login/login";
+import Register from "./pages/Login/register";
 const App = () => {
 	return (
 		<div>
@@ -14,6 +16,9 @@ const App = () => {
 					<Route path="/van-ban" element={<Layout
 						children={<Files />}
 					/>} />
+					<Route path="/login" element={<Login />} /> 
+					<Route path="/register" element={<Register />} />
+
 				</Routes>
 			</BrowserRouter>
 
