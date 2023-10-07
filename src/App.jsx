850146fc5404caa2e5c7a1e04135c0eb97e39dc7
nxhieu3102 <nguyenxuanhieuktb10@gmail.com>
2023-10-07 23:23:42 +0700

    Init redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,32 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
-import { BrowserRouter, Routes, Route, Navigate, useParams } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DocumentSearch from "./pages/DocumentSearch";
 import FileSearch from "./pages/FileSearch";
 import DocumentMetaData from "./pages/DocumentMetaData";
 import FileMetaData from "./pages/FileMetaData";
+
+const routes = [
+	{ path: "/", element: <Layout children={<Home />} /> },
+	{ path: "/van-ban", element: <Layout children={<DocumentSearch />} /> },
+	{ path: "/ho-so", element: <Layout children={<FileSearch />} /> },
+	{ path: "/van-ban/:idFile/:id", element: <Layout children={<DocumentMetaData />} /> },
+	{ path: "/ho-so/:id", element: <Layout children={<FileMetaData />} /> },
+];
 
 const App = () => {
 	return (
 		<div>
 			<BrowserRouter>
 				<Routes>
-					<Route path="/" element={<Layout
-						children={<Home />}
-					/>} />
-					<Route path="/van-ban" element={<Layout
-						children={<DocumentSearch />}
-					/>} />
-					<Route path="/ho-so" element={<Layout
-						children={<FileSearch />}
-					/>} />
-					<Route path="/van-ban/:idFile/:id" element={<Layout
-						children={<DocumentMetaData />}
-					/>} />
-					<Route path="/ho-so/:id" element={<Layout
-						children={<FileMetaData />}
-					/>} />
+					{routes.map((route, index) => (
+						<Route key={index} path={route.path} element={route.element} />
+					))}
 				</Routes>
 			</BrowserRouter>
-
 		</div>
-	)
+	);
 };
+
 export default App;
