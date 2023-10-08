adbed962c49c4c88470fd4743db98b7b620aac59
nxhieu3102 <nguyenxuanhieuktb10@gmail.com>
2023-10-08 11:31:11 +0700

    Add middleware

--- src/store.js
+++ src/store.js
@@ -1,5 +1,12 @@
-import { createStore } from "redux"
-import rootReducer from './service/reducers'
+import { createStore, applyMiddleware, compose } from "redux"; // Import compose
+import rootReducer from './service/reducers';
+import thunk from "redux-thunk";
 
-const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-export default store
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk)) 
+);
+
+export default store;
