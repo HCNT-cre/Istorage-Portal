22b3556a910746736c1836f50952aef69c65b467
nxhieu3102 <nguyenxuanhieuktb10@gmail.com>
2023-10-08 23:55:51 +0700

    Add total of doc in cart

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -28,6 +28,13 @@ const Bar = () => {
   const handleClickLogo = () => {
     navigate("/");
   };
+  const handleClickCart = () => {
+    navigate("/gio-hang");
+  }
+  
+  const cart = useSelector((state) => state.cart);
+  let total = 0
+  cart.cart.forEach((file) => total += file.docs.length)
 
   return (
     <Toolbar
@@ -54,9 +61,13 @@ const Bar = () => {
       </div>
 
       <Stack direction="row" spacing={2}>
-        <Button color="inherit" sx={buttonStyles}>
-          Giỏ tài liệu
-        </Button>
+        <div className="relative">
+          <Button color="inherit" sx={buttonStyles} onClick={handleClickCart}>
+            Giỏ tài liệu
+          </Button>
+          <span className="absolute rounded-[50%] text-[10px] bg-white text-[#3498db] p-1 w-[16px] h-[16px] left-[90%] top-0 flex justify-center items-center">{total}</span>
+        </div>
+
         <Button color="inherit" sx={buttonStyles}>
           Thông báo
         </Button>
@@ -74,9 +85,9 @@ const Header = () => {
   const sx =
     headerState === HEADER_FIXED
       ? {
-          position: "fixed",
-          backgroundColor: "transparent",
-        }
+        position: "fixed",
+        backgroundColor: "transparent",
+      }
       : null;
 
   return (
