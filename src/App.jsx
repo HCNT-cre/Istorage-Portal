575cf8ec51e95707ca4239b8e0eb0f539af2f0f8
nxhieu3102 <nguyenxuanhieuktb10@gmail.com>
2023-10-08 23:54:18 +0700

    Add cartPage and toast

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,33 @@ import DocumentSearch from "./pages/DocumentSearch";
 import FileSearch from "./pages/FileSearch";
 import DocumentMetaData from "./pages/DocumentMetaData";
 import FileMetaData from "./pages/FileMetaData";
-
+import CartPage from "./pages/Cart";
+import { ToastContainer, Zoom } from "react-toastify";
 const routes = [
 	{ path: "/", element: <Layout children={<Home />} /> },
 	{ path: "/van-ban", element: <Layout children={<DocumentSearch />} /> },
 	{ path: "/ho-so", element: <Layout children={<FileSearch />} /> },
 	{ path: "/van-ban/:idFile/:id", element: <Layout children={<DocumentMetaData />} /> },
 	{ path: "/ho-so/:id", element: <Layout children={<FileMetaData />} /> },
+	{ path: "/gio-hang", element: <Layout children={<CartPage />} /> }
 ];
 
 const App = () => {
 	return (
 		<div>
+			<ToastContainer
+				position="top-center"
+				autoClose={5000}
+				hideProgressBar={false}
+				newestOnTop={false}
+				closeOnClick
+				rtl={false}
+				pauseOnFocusLoss
+				draggable
+				pauseOnHover
+				theme="light"
+				transition={Zoom}
+			/>
 			<BrowserRouter>
 				<Routes>
 					{routes.map((route, index) => (
